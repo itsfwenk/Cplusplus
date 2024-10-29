074da83e02c2f9a9576e16111505a81714234e4c
Franck Li <fli@made-f0Br5s2.clusters.42paris.fr>
2024-10-29 17:45:01 +0100

    ex02

--- CPP01/ex02/main.cpp
+++ CPP01/ex02/main.cpp
@@ -6,34 +6,26 @@
 /*   By: fli <fli@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/29 14:42:07 by fli               #+#    #+#             */
-/*   Updated: 2024/10/29 17:32:53 by fli              ###   ########.fr       */
+/*   Updated: 2024/10/29 17:44:24 by fli              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "Zombie.hpp"
+#include <string>
+#include <iostream>
+#include <new>
+#include <sstream>
+#include <limits>
 #include <cstdio>
 
 int main()
 {
-	int			n;
-	std::string	input;
-	Zombie		*zombie_tab;
-
-	std::cout << "Enter zombie horde size : ";
-	std::cin >> input;
-	std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-	std::stringstream ss(input);
-	ss >> n;
-	while (n <= 0)
-	{
-		std::cout << "Invalid number of zombie.";
-		std::cin >> input;
-		std::stringstream ss(input);
-		ss >> n;
-		std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-	}
-	zombie_tab = zombieHorde(n, "Charles Oignon");
-	for(int i = 0; i < n; i++)
-		zombie_tab[i].announce();
-	delete[] zombie_tab;
+	std::string	str = "HI THIS IS BRAIN";
+	std::string	*stringPTR = &str;
+	std::string	&stringREF = str;
+	std::cout << "Memory address of the string variable : " << &str << std::endl;
+	std::cout << "Memory address held by stringPTR : " << stringPTR << std::endl;
+	std::cout << "Memory address held by stringREF : " << &stringREF << std::endl;
+	std::cout << "Value of the string variable : " << str << std::endl;
+	std::cout << "Value pointed to by stringPTR : " << *stringPTR << std::endl;
+	std::cout << "Value pointed to by stringREF : " << stringREF << std::endl;
 }
