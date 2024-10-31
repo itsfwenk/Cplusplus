f818d9c18556c5dbd5846f37a68fb2e7dbe85023
Franck Li <fli@made-f0Br6s5.clusters.42paris.fr>
2024-10-31 11:43:53 +0100

    ex05 06

--- CPP01/ex06/Harl.hpp
+++ CPP01/ex06/Harl.hpp
@@ -6,7 +6,7 @@
 /*   By: fli <fli@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/29 14:42:07 by fli               #+#    #+#             */
-/*   Updated: 2024/10/29 17:44:24 by fli              ###   ########.fr       */
+/*   Updated: 2024/10/31 11:41:50 by fli              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@
 # include <iostream>
 # include <string>
 # include <algorithm>
+# include <vector>
 
 class Harl
 {
