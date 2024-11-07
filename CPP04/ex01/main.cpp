c2fd0499dc18b40fa87a078295306e5c20c076dd
Franck Li <fli@made-f0Br7s4.clusters.42paris.fr>
2024-11-07 13:33:51 +0100

    cpp04 ex01

--- CPP04/ex01/main.cpp
+++ CPP04/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: fli <fli@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/05 18:52:37 by fli               #+#    #+#             */
-/*   Updated: 2024/11/06 19:17:48 by fli              ###   ########.fr       */
+/*   Updated: 2024/11/07 12:19:22 by fli              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,22 +19,31 @@
 
 int main()
 {
-	// Animal	*animalArray[10];
+	Animal	*animalArray[10];
 
-	// int i;
-	// for(i = 0; i < 5; i++)
-	// 	animalArray[i] = new Dog();
-	// for (i = 5; i < 10; i++)
-	// 	animalArray[i] = new Cat();
+	int i;
+	for(i = 0; i < 5; i++)
+		animalArray[i] = new Dog();
+	for (i = 5; i < 10; i++)
+		animalArray[i] = new Cat();
 
-	// for (i = 0; i < 10; i++)
-	// 	delete animalArray[i];
+	for (i = 0; i < 10; i++)
+		delete animalArray[i];
 
-	std::cout << "Lets copy a cat :" << std::endl;
-	Cat *cat1 = new Cat();
-	Cat *cat2 = new Cat();
-	*cat2 = *cat1;
-	delete cat1;
-	cat2->makeSound();
+	// std::cout << "Lets copy a cat :" << std::endl;
+	// Cat *cat1 = new Cat();
+	// Cat *cat2 = new Cat();
+	// *cat2 = *cat1;
+	// delete cat1;
+	// cat2->makeSound();
 
 }
+
+
+// int main()
+// {
+// const Animal* j = new Dog();
+// const Animal* i = new Cat();
+// delete j;//should not create a leak
+// delete i;
+// }
