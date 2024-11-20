861916f4ab0b1ce4a40f54b463fd80ba4d884d44
Franck Li <fli@made-f0Br7s7.clusters.42paris.fr>
2024-11-20 11:39:54 +0100

    fix cpp07

--- CPP07/ex01/iter.hpp
+++ CPP07/ex01/iter.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   iter.hpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mli <mli@student.42.fr>                    +#+  +:+       +#+        */
+/*   By: fli <fli@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/17 15:39:27 by mli               #+#    #+#             */
-/*   Updated: 2024/11/17 16:29:41 by mli              ###   ########.fr       */
+/*   Updated: 2024/11/20 11:26:45 by fli              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,20 @@
 
 template <typename T>
 void iter(T* array, size_t length, void (*func)(T))
+{
+	for (size_t i = 0; i < length; i++)
+		func(array[i]);
+}
+
+template <typename T>
+void iter(T* array, size_t length, void (*func)(T&))
+{
+	for (size_t i = 0; i < length; i++)
+		func(array[i]);
+}
+
+template <typename T>
+void iter(T* array, size_t length, void (*func)(const T&))
 {
 	for (size_t i = 0; i < length; i++)
 		func(array[i]);
