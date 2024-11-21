747a3deee50014c20e940d1513664dedc6919f8f
Franck Li <fli@made-f0Br6s5.clusters.42paris.fr>
2024-11-21 11:09:16 +0100

    cpp06

--- CPP08/ex02/MutantStack.hpp
+++ CPP08/ex02/MutantStack.hpp
@@ -6,7 +6,7 @@
 /*   By: fli <fli@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/20 17:55:09 by fli               #+#    #+#             */
-/*   Updated: 2024/11/20 19:58:00 by fli              ###   ########.fr       */
+/*   Updated: 2024/11/21 11:00:08 by fli              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,11 +24,11 @@ class MutantStack : public std::stack<T, std::deque<T> >
 	public:
 		MutantStack() : std::stack<T>() {};
 		~MutantStack() {};
-		MutantStack (const MutantStack &other) :
+		MutantStack (const MutantStack &other): std::stack<T>()
 		{
 			this->c = other.c;
 		};
-		MutantStack & operator=(const MutantStack &value)
+		MutantStack & operator=(const MutantStack &other)
 		{
 			if (this != &other)
 			{
@@ -39,10 +39,26 @@ class MutantStack : public std::stack<T, std::deque<T> >
 
 		typedef typename std::deque<T>::iterator		iterator;
 		typedef typename std::deque<T>::const_iterator	const_iterator;
-		const const_iterator begin() const;
-		const const_iterator end() const;
-		iterator begin();
-		iterator end();
+
+		const const_iterator begin() const
+		{
+			return this->c.begin();
+		};
+
+		const const_iterator end() const
+		{
+			return this->c.end();
+		};
+
+		iterator begin()
+		{
+			return this->c.begin();
+		};
+
+		iterator end()
+		{
+			return this->c.end();
+		};
 };
 
 #endif
---
+++ CPP08/ex02/main.cpp
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fli <fli@student.42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/21 10:57:56 by fli               #+#    #+#             */
+/*   Updated: 2024/11/21 10:58:50 by fli              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "MutantStack.hpp"
+
+int main()
+{
+	MutantStack<int> mstack;
+	mstack.push(5);
+	mstack.push(17);
+	std::cout << mstack.top() << std::endl;
+	mstack.pop();
+	std::cout << mstack.size() << std::endl;
+	mstack.push(3);
+	mstack.push(5);
+	mstack.push(737);
+	//[...]
+	mstack.push(0);
+	MutantStack<int>::iterator it = mstack.begin();
+	MutantStack<int>::iterator ite = mstack.end();
+	++it;
+	--it;
+	while (it != ite)
+	{
+	std::cout << *it << std::endl;
+	++it;
+	}
+	std::stack<int> s(mstack);
+	return 0;
+}
